e23e4df1573d6f62397f6044cc046065ca0c7d34
Hoang Duc Dai Hanh <hanhhdd@bap.jp>
2022-11-22 08:43:47 +0700

    back to game

--- function/HorseFarm.js
+++ function/HorseFarm.js
@@ -1,171 +1,178 @@
-const ABIHorseFarm = horseFarm
-const ABIHorseNFT = horseNFT
-const HORSENFT_ADDRESS = ''
+const ABIHorseFarm = horseFarm;
+const ABIHorseNFT = horseNFT;
+const HORSENFT_ADDRESS = "";
 
 const sign = async (message) => {
-    let web3 = new Web3(window.ethereum)
-    let accounts = await web3.eth.getAccounts();
-    console.log("accounts: ", accounts[0]);
-    let signature = await web3.eth.personal.sign(message, accounts[0], '');
-    console.log('sign: ', [accounts, message, signature].join('|'));
+  let web3 = new Web3(window.ethereum);
+  let accounts = await web3.eth.getAccounts();
+  console.log("accounts: ", accounts[0]);
+  let signature = await web3.eth.personal.sign(message, accounts[0], "");
+  console.log("sign: ", [accounts, message, signature].join("|"));
 
-    document.getElementById("p1").innerHTML = "Login success! Copy and go back your game!";
-    createCopyInputButton([accounts, message, signature].join('|'));
-  }
+  document.getElementById("p1").innerHTML =
+    "Login success! Copy and go back your game!";
+  createCopyInputButton([accounts, message, signature].join("|"));
+};
 
 //user lease horse
-async function lease(data, token_id){
-    let web3 = new Web3(window.ethereum)
-    const HorseFarmContract = new web3.eth.Contract(ABIHorseFarm, data.horse_farm_address)
-    const HorseNFTContract = new web3.eth.Contract(ABIHorseNFT, HORSENFT_ADDRESS)
+async function lease(data, token_id) {
+  let web3 = new Web3(window.ethereum);
+  const HorseFarmContract = new web3.eth.Contract(
+    ABIHorseFarm,
+    data.horse_farm_address
+  );
+  const HorseNFTContract = new web3.eth.Contract(ABIHorseNFT, HORSENFT_ADDRESS);
 
-    await HorseNFTContract.methods.approve(data.transporter_address, token_id).send({
+  await HorseNFTContract.methods
+    .approve(data.transporter_address, token_id)
+    .send(
+      {
         from: data.owner,
         to: HORSENFT_ADDRESS,
         gasLimit: web3.utils.toHex("1000000"),
-        gasPrice: await getGasPrice()
-        },function (err, res) {
-            if (err) {
-              console.log("An error occured", err)
-              return
-            }
-        })
-    
-    await HorseFarmContract.methods.lease({
-        owner: data.owner,
-        horseId: token_id,
-        blockExpired: data.block_expired,
-        nonce: data.nonce,
-        v: data.v,
-        r: data.r,
-        s: data.s
-    }).send({
+        gasPrice: await getGasPrice(),
+      },
+      function (err, res) {
+        if (err) {
+          console.log("An error occured", err);
+          return;
+        }
+      }
+    );
+
+  await HorseFarmContract.methods
+    .lease({
+      owner: data.owner,
+      horseId: token_id,
+      blockExpired: data.block_expired,
+      nonce: data.nonce,
+      v: data.v,
+      r: data.r,
+      s: data.s,
+    })
+    .send(
+      {
         from: data.owner,
         to: data.horse_farm_address,
         gasLimit: web3.utils.toHex("1000000"),
-        gasPrice: await getGasPrice()
-    },function (err, res) {
+        gasPrice: await getGasPrice(),
+      },
+      function (err, res) {
         if (err) {
-          console.log("An error occured", err)
-          return
+          console.log("An error occured", err);
+          return;
         }
-        console.log("Hash of the transaction: " + res)
-        document.getElementById("p1").innerHTML = "lease success! Copy and go back your game!";
+        console.log("Hash of the transaction: " + res);
+        document.getElementById("p1").innerHTML =
+          "lease success! Copy and go back your game!";
         createCopyInputButton(res);
-    })
+      }
+    );
 }
 
 //user withdraw horse
-async function withdraw(data, token_id){
-    let web3 = new Web3(window.ethereum)
-    const HorseFarmContract = new web3.eth.Contract(ABIHorseFarm, data.horse_farm_address)
-    await HorseFarmContract.methods.withdraw({
-        owner: data.owner,
-        horseId: token_id,
-        blockExpired: data.block_expired,
-        nonce: data.nonce,
-        v: data.v,
-        r: data.r,
-        s: data.s
-    }).send({
+async function withdraw(data, token_id) {
+  let web3 = new Web3(window.ethereum);
+  const HorseFarmContract = new web3.eth.Contract(
+    ABIHorseFarm,
+    data.horse_farm_address
+  );
+  await HorseFarmContract.methods
+    .withdraw({
+      owner: data.owner,
+      horseId: token_id,
+      blockExpired: data.block_expired,
+      nonce: data.nonce,
+      v: data.v,
+      r: data.r,
+      s: data.s,
+    })
+    .send(
+      {
         from: data.owner,
         to: data.horse_farm_address,
         gasLimit: web3.utils.toHex("1000000"),
-        gasPrice: await getGasPrice()
-    },function (err, res) {
+        gasPrice: await getGasPrice(),
+      },
+      function (err, res) {
         if (err) {
-          console.log("An error occured", err)
-          return
+          console.log("An error occured", err);
+          return;
         }
-        console.log("Hash of the transaction: " + res)
-        document.getElementById("p1").innerHTML = "withdraw success! Copy and go back your game!";
+        console.log("Hash of the transaction: " + res);
+        document.getElementById("p1").innerHTML =
+          "withdraw success! Copy and go back your game!";
         createCopyInputButton(res);
-    })
+      }
+    );
 }
 
-async function getGasPrice(){
-    return await web3.eth.getGasPrice();
+async function getGasPrice() {
+  return await web3.eth.getGasPrice();
 }
 
 window.onload = async () => {
-    console.log(123);
-    if (window.ethereum) {
-      await window.ethereum.request({ method: "eth_requestAccounts" });
-      window.web3 = new Web3(window.ethereum);
-    } else {
-      alert("Please install MetaMask Extension in your browser");
-    }
-  
-    const params = new URLSearchParams(window.location.search)
-  
-    console.log(params)
-    console.log(params.get('action'))
-    console.log(params.get('data'))
-  
-    switch(params.get('action'))
-    {
-        case "sign":
-        sign(params.get('data'));
-            break;
-        case "lease":
-          lease(JSON.parse(params.get('data')));
-          break;
-        case "withdraw":
-            withdraw(JSON.parse(params.get('data')))
-            break;
-      default:
-          break;
-    }
-  };
+  console.log(123);
+  if (window.ethereum) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    window.web3 = new Web3(window.ethereum);
+  } else {
+    alert("Please install MetaMask Extension in your browser");
+  }
 
-  const createCopyInputButton = (data) => {
-    var btnCopy = document.createElement('input');
-    btnCopy.type = "button";
-    btnCopy.id = "btnCopy";
-    btnCopy.value = "Return Game";
-  
-    btnCopy.onclick = () => copyToClipboard(data);
-    document.body.appendChild(btnCopy);
-    copyToClipboard(data)
+  const params = new URLSearchParams(window.location.search);
+
+  console.log(params);
+  console.log(params.get("action"));
+  console.log(params.get("data"));
+
+  switch (params.get("action")) {
+    case "sign":
+      sign(params.get("data"));
+      break;
+    case "lease":
+      lease(JSON.parse(params.get("data")));
+      break;
+    case "withdraw":
+      withdraw(JSON.parse(params.get("data")));
+      break;
+    default:
+      break;
+  }
+};
+
+const createCopyInputButton = (data) => {
+  var btnCopy = document.createElement("input");
+  btnCopy.type = "button";
+  btnCopy.id = "btnCopy";
+  btnCopy.value = "Return Game";
+
+  btnCopy.onclick = () => copyToClipboard(data);
+  document.body.appendChild(btnCopy);
+  copyToClipboard(data);
+  // openMetaHorse()
+};
+
+const copyToClipboard = async function (data) {
+  try {
+    // focus from metamask back to browser
+    window.focus();
+    // wait to finish focus
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    // copy tx hash to clipboard
+    await navigator.clipboard.writeText(data);
+    document.getElementById("p1").innerHTML = data;
     // openMetaHorse()
+  } catch (err) {
+    console.log(err);
+    // for metamask mobile android
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = data;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("Copy");
+    input.style = "visibility: hidden";
+    document.getElementById("p1").innerHTML = data;
   }
-
-  const copyToClipboard = async function(data) {
-    try {
-      // focus from metamask back to browser
-      window.focus();
-      // wait to finish focus
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      // copy tx hash to clipboard
-      await navigator.clipboard.writeText(data);
-      document.getElementById("p1").innerHTML = data;
-      openMetaHorse()
-
-    } catch(err) {
-      console.log(err);
-      // for metamask mobile android
-      const input = document.createElement("input");
-      input.type = "text";
-      input.value = data;
-      document.body.appendChild(input);
-      input.select();
-      document.execCommand("Copy");
-      input.style = "visibility: hidden";
-      document.getElementById("p1").innerHTML = data;
-    }
-  }
-
-  function isMobileDevice() {
-    return "ontouchstart" in window || "onmsgesturechange" in window;
-  }
-
-  function openMetaHorse() {
-    console.log('1')
-    if (isMobileDevice()) {
-        console.log('2')
-        window.open("metahorse://web3login");
-    } else {
-        console.log('3')
-        window.open("https://metamask.io/", "_blank");
-    }
-  }
+};
